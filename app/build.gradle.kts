0a5214e55eaa36241cc1095949b1edae863507a0
Pat Yeh <yehforcode@gmail.com>
2026-08-27 19:12:37 +0800

    Bump to 0.19.2 (versionCode 26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.routina.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.19.1"
+        versionCode = 26
+        versionName = "0.19.2"
 
         vectorDrawables {
             useSupportLibrary = true
