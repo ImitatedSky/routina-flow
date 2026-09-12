df925b6e48e0c2928c59d2a2a6001153bed28933
Pat Yeh <yehforcode@gmail.com>
2026-09-13 00:03:55 +0800

    Release v0.23.0: routine export and import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.routina.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "0.22.0"
+        versionCode = 32
+        versionName = "0.23.0"
 
         vectorDrawables {
             useSupportLibrary = true
